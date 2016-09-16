ad70d7e5fb82db608960038328373bad544dccd4
dtkaplan <dtkaplan@gmail.com>
2016-09-15 20:02:28 -0500

    Final exam scheduled

---
+++ Ephemera/Day-4.R
@@ -0,0 +1,23 @@
+# Thursday Sept. 15, 2016
+
+## Don't use `fix()`
+
+Use `View()` instead.
+```r
+View(College)
+```
+
+## Subsets
+
+The book has you use `fix()`.
+
+```{r}
+library(ISLR)
+data(Auto)
+head(Auto)
+```
+
+### Missing data
+
+```{r}
+```
